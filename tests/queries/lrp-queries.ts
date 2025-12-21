d57ae789976571e3ba427618a7ddb5a43f2310cc
brokri-lgtm <>
2025-12-21 22:57:14 +0100

    refactor cdp queries + add lrp query

--- tests/queries/lrp-queries.ts
+++ tests/queries/lrp-queries.ts
@@ -1,12 +1,17 @@
 import {
   Credential,
   LucidEvolution,
-  Network,
   ScriptHash,
   UTxO,
 } from '@lucid-evolution/lucid';
 import { createScriptAddress } from '../../src/utils/lucid-utils';
-import { parseLrpDatum } from '../../src/contracts/lrp/types';
+import {
+  LRPDatum,
+  parseLrpDatum,
+  parseLrpDatumOrThrow,
+} from '../../src/contracts/lrp/types';
+import { SystemParams } from '../../src';
+import { option as O, array as A, function as F } from 'fp-ts';
 
 /**
  * Beware, this shouldn't be used in production since it queries all the UTXOs
@@ -14,12 +19,13 @@ import { parseLrpDatum } from '../../src/contracts/lrp/types';
  */
 export async function findLrp(
   lucid: LucidEvolution,
-  network: Network,
   lrpScriptHash: ScriptHash,
   owner: string,
   assetTokenName: string,
   stakeCredential?: Credential,
 ): Promise<UTxO[]> {
+  const network = lucid.config().network!;
+
   const lrpUtxos = await lucid.utxosAt(
     createScriptAddress(network, lrpScriptHash, stakeCredential),
   );
@@ -27,7 +33,7 @@ export async function findLrp(
   return lrpUtxos.filter((utxo) => {
     if (utxo.datum != null) {
       try {
-        const lrpDatum = parseLrpDatum(utxo.datum);
+        const lrpDatum = parseLrpDatumOrThrow(utxo.datum);
 
         return lrpDatum.owner == owner && lrpDatum.iasset == assetTokenName;
       } catch (_) {
@@ -36,4 +42,35 @@ export async function findLrp(
       }
     }
   });
+}
+
+export async function findAllLrps(
+  lucid: LucidEvolution,
+  sysParams: SystemParams,
+  // hex encoded
+  iasset: string,
+): Promise<{ utxo: UTxO; datum: LRPDatum }[]> {
+  const lrpUtxos = await lucid.utxosAt(
+    createScriptAddress(
+      lucid.config().network!,
+      sysParams.validatorHashes.lrpHash,
+    ),
+  );
+
+  return F.pipe(
+    lrpUtxos.map((utxo) =>
+      F.pipe(
+        O.fromNullable(utxo.datum),
+        O.flatMap(parseLrpDatum),
+        O.flatMap((datum) => {
+          if (datum.iasset === iasset) {
+            return O.some({ utxo, datum: datum });
+          } else {
+            return O.none;
+          }
+        }),
+      ),
+    ),
+    A.compact,
+  );
 }
